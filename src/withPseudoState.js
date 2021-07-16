f4dcd6896a00766993e99fd825bf77ebd77df064
Jamie Rolfs <jamie.rolfs@gmail.com>
2021-07-16 08:56:36 -0700

    Add support for `Canvas` docs block in psuedo state decorator
    Fixes #11

--- src/withPseudoState.js
+++ src/withPseudoState.js
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 import { addons, useEffect, useGlobals, useParameter } from "@storybook/addons"
-import { STORY_CHANGED, STORY_RENDERED } from "@storybook/core-events"
+import { DOCS_RENDERED, STORY_CHANGED, STORY_RENDERED } from "@storybook/core-events"
 
 import { PSEUDO_STATES } from "./constants"
 
@@ -37,26 +37,37 @@ const shadowHosts = new Set()
 addons.getChannel().on(STORY_CHANGED, () => shadowHosts.clear())
 
 // Global decorator that rewrites stylesheets and applies classnames to render pseudo styles
-export const withPseudoState = (StoryFn) => {
-  const parameter = useParameter("pseudo")
+export const withPseudoState = (StoryFn, { viewMode, parameters, id }) => {
+  const isDocs = viewMode === 'docs';
+  const { pseudo: parameter } = parameters;
   const [{ pseudo: globals }, updateGlobals] = useGlobals()
 
-  // Sync parameter to globals, used by the toolbar
+  // Sync parameter to globals, used by the toolbar (only in canvas as this
+  // doesn't make sense for docs because many stories are displayed at once)
   useEffect(() => {
-    if (parameter !== globals) updateGlobals({ pseudo: parameter })
-  }, [parameter])
+    if (parameter !== globals && !isDocs) updateGlobals({ pseudo: parameter })
+  }, [parameter, isDocs])
 
   // Convert selected states to classnames and apply them to the story root element.
   // Then update each shadow host to redetermine its own pseudo classnames.
   useEffect(() => {
-    applyClasses(
-      document.getElementById("root"),
-      Object.entries(globals || {})
-        .filter(([_, value]) => value)
-        .map(([key]) => `pseudo-${PSEUDO_STATES[key]}`)
-    )
-    shadowHosts.forEach(updateShadowHost)
-  }, [globals])
+    const apply = element => {
+      applyClasses(
+        element,
+        Object.entries(parameter || {})
+          .filter(([_, value]) => value)
+          .map(([key]) => `pseudo-${PSEUDO_STATES[key]}`)
+      )
+      shadowHosts.forEach(updateShadowHost)
+    }
+
+    if (isDocs) {
+      // Wait for the docs page to render so we can select the story element 😑
+      setTimeout(() => apply(document.getElementById(`story--${id}`)), 0);
+    } else {
+      apply(document.getElementById("root"));
+    }
+  }, [parameter])
 
   return StoryFn()
 }
@@ -117,6 +128,9 @@ function rewriteStyleSheets(shadowRoot) {
 
 // Reinitialize CSS enhancements every time the story changes
 addons.getChannel().on(STORY_RENDERED, () => rewriteStyleSheets())
+
+// Reinitialize CSS enhancements every time a docs page is rendered
+addons.getChannel().on(DOCS_RENDERED, () => rewriteStyleSheets())
 
 // IE doesn't support shadow DOM
 if (Element.prototype.attachShadow) {
