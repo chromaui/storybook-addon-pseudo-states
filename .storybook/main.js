0ce6c0b7865858358e8b5df9096b11ae68f83d8c
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2023-03-17 20:00:13 +0000

    feat: Upgrade to storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,11 @@
 module.exports = {
-  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-docs", "../preset.js"],
-  core: {
-    builder: "webpack5",
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-docs", "../dist/preset.js"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
-}
+  docs: {
+    autodocs: true
+  }
+};
