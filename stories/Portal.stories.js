c806785156cfa47f16b750e33f4c07fc8f48059b
Gert Hengeveld <info@ghengeveld.nl>
2023-06-22 15:06:24 +0200

    Rename rootElement param to rootSelector, and fix memoization/state updates

--- stories/Portal.stories.js
+++ stories/Portal.stories.js
@@ -14,16 +14,16 @@ const PortalButton = (props) => createPortal(<Button {...props} />, document.bod
 const Template = (args) => <PortalButton {...args}>Label</PortalButton>
 
 export const Default = Template.bind()
-Default.parameters = { pseudo: { rootElement: "body" } }
+Default.parameters = { pseudo: { rootSelector: "body" } }
 
 export const Hover = Template.bind()
-Hover.parameters = { pseudo: { hover: true, rootElement: "body" } }
+Hover.parameters = { pseudo: { hover: true, rootSelector: "body" } }
 
 export const Focus = Template.bind()
-Focus.parameters = { pseudo: { focus: true, rootElement: "body" } }
+Focus.parameters = { pseudo: { focus: true, rootSelector: "body" } }
 
 export const Active = Template.bind()
-Active.parameters = { pseudo: { active: true, rootElement: "body" } }
+Active.parameters = { pseudo: { active: true, rootSelector: "body" } }
 
 export const FocusedHover = Template.bind()
-FocusedHover.parameters = { pseudo: { focus: true, hover: true, rootElement: "body" } }
+FocusedHover.parameters = { pseudo: { focus: true, hover: true, rootSelector: "body" } }
