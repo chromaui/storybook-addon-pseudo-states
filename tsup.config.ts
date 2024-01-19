88899915a6fdb39ceeea490130e79e570c93d9f9
Shaun Lloyd <shaun@chromatic.com>
2024-01-19 15:23:35 -0500

    Update build settings

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "tsup"
 
-export default defineConfig(() => {
+export default defineConfig((options) => {
   return {
     dts: { entry: ["src/index.ts", "src/manager.ts", "src/preview.ts"] },
     entry: ["src/index.ts", "src/manager.ts", "src/preview.ts"],
     format: ["esm", "cjs"],
+    splitting: false,
+    minify: !options.watch,
+    treeshake: true,
+    sourcemap: true,
+    clean: true,
+    platform: "browser",
+    esbuildOptions(options) {
+      options.conditions = ["module"];
+    },
   }
 })
