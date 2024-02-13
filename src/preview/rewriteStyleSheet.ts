a2c38891464020b4973a3a15e3c903a8224dd399
m-akinc <7282195+m-akinc@users.noreply.github.com>
2024-02-12 20:32:44 -0600

    Minor fix

--- src/preview/rewriteStyleSheet.ts
+++ src/preview/rewriteStyleSheet.ts
@@ -41,21 +41,20 @@ const rewriteRule = ({ cssText, selectorText }: CSSStyleRule, shadowRoot?: Shado
         let classAllSelector = ""
         let ancestorSelector = ""
         const statesAllClassSelectors = states.map((s) => `.pseudo-${s}-all`).join("")
-        if (shadowRoot) {
-          if (selector.startsWith(":host(") || selector.startsWith("::slotted(")) {
-            const matches = selector.match(/^:host\(([^ ]+)\) /)
-            if (matches && !matchOne.test(matches[1])) {
-              // If :host() did not contain states, then classAllSelector won't work, and we need this selector.
-              ancestorSelector = `:host(${matches[1]}${statesAllClassSelectors}) ${plainSelector.replace(matches[0], "")}`
-            } else {
-              classAllSelector = states.reduce((acc, state) => {
-                if (isExcludedPseudoElement(selector, state)) return ""
-                return acc.replace(new RegExp(`:${state}`, "g"), `.pseudo-${state}-all`)
-              }, selector)
-            }
+        
+        if (selector.startsWith(":host(") || selector.startsWith("::slotted(")) {
+          const matches = selector.match(/^:host\(([^ ]+)\) /)
+          if (matches && !matchOne.test(matches[1])) {
+            // If :host() did not contain states, then classAllSelector won't work, and we need this selector.
+            ancestorSelector = `:host(${matches[1]}${statesAllClassSelectors}) ${plainSelector.replace(matches[0], "")}`
           } else {
-            ancestorSelector = `:host(${statesAllClassSelectors}) ${plainSelector}`
+            classAllSelector = states.reduce((acc, state) => {
+              if (isExcludedPseudoElement(selector, state)) return ""
+              return acc.replace(new RegExp(`:${state}`, "g"), `.pseudo-${state}-all`)
+            }, selector)
           }
+        } else if (shadowRoot) {
+          ancestorSelector = `:host(${statesAllClassSelectors}) ${plainSelector}`
         } else {
           ancestorSelector = `${statesAllClassSelectors} ${plainSelector}`
         }
