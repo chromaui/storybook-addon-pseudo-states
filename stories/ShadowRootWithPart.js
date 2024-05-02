a8f043728f1978aaae922df13f6ffad8960e835e
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-05-02 15:05:00 -0500

    Fix ShadowRootWithPart story

--- stories/ShadowRootWithPart.js
+++ stories/ShadowRootWithPart.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./ShadowRootWithPart.css"
 
 export const ShadowRoot = ({ label = "Hello from shadow DOM" }) => {
   const ref = React.useRef()
@@ -8,33 +9,6 @@ export const ShadowRoot = ({ label = "Hello from shadow DOM" }) => {
     ref.current.attachShadow({ mode: "closed" })
     ref.current.shadowRoot.innerHTML = `
       <button part="foo">${label}</button>
-    `
-    ref.current.innerHTML = `
-      <style>
-        ::part(foo) {
-          font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-          font-weight: 700;
-          border: 0;
-          border-radius: 3em;
-          cursor: pointer;
-          display: inline-block;
-          line-height: 1;
-          color: white;
-          background-color: tomato;
-          font-size: 14px;
-          padding: 11px 20px;
-        }
-        ::part(foo):hover {
-          text-decoration: underline;
-        }
-        ::part(foo):focus {
-          box-shadow: inset 0 0 0 2px maroon;
-          outline: 0;
-        }
-        ::part(foo):active {
-          background-color: firebrick;
-        }
-      </style>
     `
   }, [])
 
