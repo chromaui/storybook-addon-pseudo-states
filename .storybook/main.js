2ae3e8284de0db23305c665cee7bb0afa47d9231
Gert Hengeveld <info@ghengeveld.nl>
2024-08-29 11:30:35 +0200

    Remove unused addons and upgrade TypeScript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,7 @@
 module.exports = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@chromatic-com/storybook",
-  ],
+  addons: ["@chromatic-com/storybook"],
+  framework: "@storybook/react-vite",
   managerEntries: [__dirname + "/../src/manager.ts"],
   previewAnnotations: [__dirname + "/../src/preview.ts"],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    docsPage: true,
-  },
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 }
