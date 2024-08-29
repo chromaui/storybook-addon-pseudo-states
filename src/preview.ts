40cdb933ed1ecfff965496c08ee9ae0056c8edb4
Gert Hengeveld <info@ghengeveld.nl>
2024-08-29 11:52:44 +0200

    Use initialGlobals instead of globals

--- src/preview.ts
+++ src/preview.ts
@@ -2,4 +2,4 @@ import { PARAM_KEY } from "./constants"
 import { withPseudoState } from "./preview/withPseudoState"
 
 export const decorators = [withPseudoState]
-export const globals = { [PARAM_KEY]: false }
+export const initialGlobals = { [PARAM_KEY]: false }
