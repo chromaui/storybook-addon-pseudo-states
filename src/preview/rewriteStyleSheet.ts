4844789f7b81b18cfe926c064b9a2628d20e18f5
Victor Tortolero <victor.tortolero@shopify.com>
2025-03-20 06:39:57 -0600

    Do not replace :is() with :is(*)

--- src/preview/rewriteStyleSheet.ts
+++ src/preview/rewriteStyleSheet.ts
@@ -50,7 +50,8 @@ const extractPseudoStates = (selector: string) => {
       return ""
     })
     // If removing pseudo-state selectors from inside a functional selector left it empty (thus invalid), must fix it by adding '*'.
-    .replaceAll("()", "(*)")
+    // The negative lookbehind ensures we don't replace :is() with :is(*).
+    .replaceAll(/(?<!is)\(\)/g, "(*)")
     // If a selector list was left with blank items (e.g. ", foo, , bar, "), remove the extra commas/spaces.
     .replace(/(?<=[\s(]),\s+|(,\s+)+(?=\))/g, "") || "*"
 
